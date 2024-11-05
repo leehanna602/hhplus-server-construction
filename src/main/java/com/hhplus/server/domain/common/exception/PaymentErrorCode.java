9254c80706afbf1cf79f055e2d7eeeb1b6c68e83
hanna <2005haba98@gmail.com>
2024-11-05 12:00:07 +0900

    refactor: 에러코드 message 오타 수정

--- src/main/java/com/hhplus/server/domain/common/exception/PaymentErrorCode.java
+++ src/main/java/com/hhplus/server/domain/common/exception/PaymentErrorCode.java
@@ -18,7 +18,7 @@ public enum PaymentErrorCode implements ErrorCode{
     POINT_CHARGE_LOCK_ACQUISITION_FAILURE(HttpStatus.INTERNAL_SERVER_ERROR, "포인트 충전 락 획득 실패", ERROR),
     POINT_USE_FAILURE(HttpStatus.INTERNAL_SERVER_ERROR, "포인트 사용 중 에러 발생", ERROR),
     POINT_CHARGE_FAILURE(HttpStatus.INTERNAL_SERVER_ERROR, "포인트 사용 중 에러 발생", ERROR),
-    FAILED_PAYMENT(HttpStatus.INTERNAL_SERVER_ERROR, "결재에 실패했습니다.", ERROR)
+    FAILED_PAYMENT(HttpStatus.INTERNAL_SERVER_ERROR, "결제에 실패했습니다.", ERROR)
     ;
 
     private final HttpStatus status;
