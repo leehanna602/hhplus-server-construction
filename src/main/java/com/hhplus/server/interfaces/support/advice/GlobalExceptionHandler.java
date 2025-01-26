e7ae8e1001653ca17acf63831b2f75d7bdb24616
hanna <2005haba98@gmail.com>
2025-01-26 22:53:25 +0900

    refactor: ExceptionHandler 불필요한 코드 정리

--- src/main/java/com/hhplus/server/interfaces/support/advice/GlobalExceptionHandler.java
+++ src/main/java/com/hhplus/server/interfaces/support/advice/GlobalExceptionHandler.java
@@ -3,8 +3,6 @@ package com.hhplus.server.interfaces.support.advice;
 import com.hhplus.server.domain.support.exception.ErrorCode;
 import com.hhplus.server.domain.support.exception.CommonException;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,8 +13,6 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    private final Logger logger = LoggerFactory.getLogger(getClass());
-
     @ExceptionHandler(value = CommonException.class)
     public ResponseEntity<ErrorResponse> handleException(CommonException e) {
         switch (e.getErrorCode().getLogLevel()) {
@@ -24,18 +20,29 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
             case WARN -> log.warn("CommonException : {}", e.getMessage(), e);
             default -> log.info("CommonException : {}", e.getMessage(), e);
         }
+
         ErrorCode errorCode = e.getErrorCode();
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .status(errorCode.getStatus())
+                .name(errorCode.getStatus().name())
+                .message(errorCode.getMessage())
+                .build();
+
         return ResponseEntity
                 .status(errorCode.getStatus())
-                .body(new ErrorResponse(errorCode.getStatus(), errorCode.getStatus().name(), errorCode.getMessage()));
+                .body(errorResponse);
     }
 
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
         log.error("Exception: ", e);
         return ResponseEntity
-                .status(500)
-                .body(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR.name(), "에러가 발생했습니다."));
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ErrorResponse.builder()
+                        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .name(HttpStatus.INTERNAL_SERVER_ERROR.name())
+                        .message("에러가 발생했습니다.")
+                        .build());
     }
 
 }
